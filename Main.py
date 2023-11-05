415df00399df4449e9564c617ea7fd02456ec9b5
SONU KOL <108781856+LinkNatureAi@users.noreply.github.com>
2023-11-05 05:58:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,26 +7,38 @@ client = mqtt.Client()
 
 # Define the callback function for when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
-  print("Connected with result code " + str(rc))
-  # Subscribe to the topic "train"
-  client.subscribe("train")
+  if rc == 0:
+    print("Connected successfully")
+    # Subscribe to the topic "train"
+    client.subscribe("train")
+  else:
+    print("Connection failed with code " + str(rc))
 
 # Define the callback function for when the client receives a message
 def on_message(client, userdata, msg):
-  print("Received message: " + msg.topic + " -> " + msg.payload.decode())
+  try:
+    print("Received message: " + msg.topic + " -> " + msg.payload.decode())
+  except Exception as e:
+    print("Error handling message:", e)
 
-# Connect to the HiveMQ broker
-client.connect("broker.hivemq.com", 1883, 60)
+# Define the callback function for when there's an error
+def on_error(client, userdata, rc):
+  print("Error: " + str(rc))
 
-# Set the callback functions
-client.on_connect = on_connect
-client.on_message = on_message
+# Connect to the HiveMQ broker with error handling
+try:
+  client.on_connect = on_connect
+  client.on_message = on_message
+  client.on_error = on_error
+  client.connect("broker.hivemq.com", 1883, 60)
+  client.loop_start()
 
-# Start the loop to process MQTT messages
-client.loop_start()
+  # Publish a random number to the topic "train" every 3 seconds
+  while True:
+    random_number = random.randint(1, 100)
+    client.publish("train2", str(random_number))
+    time.sleep(0.5)
 
-# Publish a random number to the topic "train" every 3 seconds
-while True:
-  random_number = random.randint(1, 100)
-  client.publish("train2", str(random_number))
-  time.sleep(0.5)
+except Exception as e:
+  print("Error:", e)
+  client.disconnect()
